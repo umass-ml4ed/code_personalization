4a9760e86b0fb28d0d14f0f2ca6aadbfd94be758
Gavin <gavin-duan@qq.com>
2025-12-24 11:01:28 -0500

    Update Error Coverage Reward
    Change error coverage reward from intersection over ground truth submissions error list to IOU

--- Ability Aligned Code Generation/grpo.py
+++ Ability Aligned Code Generation/grpo.py
@@ -28,6 +28,32 @@ def read_data(args, test_size=0.2, random_state=0):
     # error_df = pd.read_pickle('data/data_inc_error_label_full.pkl')
     # df = df.merge(error_df[['SubjectID', 'ServerTimestamp', 'error_labels']], on=['SubjectID', 'ServerTimestamp'], how='left')
     
+    ## map the problem level error lists to each student attempt
+    ## map each individual error list to the mapped cluster error name for all submissions
+    # problem_error_dict = pd.read_pickle('data/problem_error_dict_30.pkl')
+
+    # df['problem_error_labels'] = df['prompt'].map(problem_error_dict)
+
+    # map_dict = pd.read_pickle('data/error_name_mapping_30.pkl')
+
+    # def clean_error_labels(row):
+    #     problem = row['prompt']
+    #     error_list = row['error_labels']
+    #     if len(error_list) == 0:
+    #         return []
+        
+    #     error_ls_i = list(set([item[-1] for item in error_list]))
+    #     clean_errors = [re.sub(r'(?<!^)(?<!-)(?=[A-Z])', ' ', s) for s in error_ls_i]
+    #     clean_errors = [re.sub(r"\s+", " ", s) for s in clean_errors]
+    #     clean_errors = [s.lower().strip() for s in clean_errors]
+    #     clean_errors = [s[:-1] if s.endswith('s') and not s.endswith('ss') else s for s in clean_errors]
+
+    #     mapping = map_dict[problem]
+    #     mapped_errors = [mapping[s] for s in clean_errors]
+
+    #     return mapped_errors
+
+    # df['error_labels'] = df.apply(clean_error_labels, axis=1)
 
     if not args.with_knowledge:
         problem_to_codes = df.groupby("prompt")["Code"].apply(list).to_dict()
@@ -93,7 +119,8 @@ def load_student_ability_dataset(df, tokenizer, prompt_column = "prompt"):
         problem = ex['prompt']
         kc_ls = ex['knowledge_component']
 
-        errors = list(set([item[-1] for item in ex['error_labels']]))
+        errors = ex['error_labels']
+        problem_errors = ex['problem_error_labels']
         
         prompt = "Problem: " + problem + "\n\nStudent information:"
         for idx, (kc_i, kc_ability) in enumerate(zip(kc_ls, kc_ability_list)):
@@ -118,7 +145,8 @@ def load_student_ability_dataset(df, tokenizer, prompt_column = "prompt"):
         out = {"prompt": prompt,
                "problem": problem,
                "code": ex["Code"],
-               "errors": errors
+               "errors": errors,
+               "problem_errors": problem_errors
         }
         
         return out
@@ -169,18 +197,37 @@ def diversity_reward(completions, **kwargs):
 
 def make_error_reward_fn(model, tokenizer):
     @torch.inference_mode()
-    def contains_error(outputs, problems, error_ls):
-        system_content = """You are an experienced code reviewer. Given a programming problem along with a code and a list of errors, your task is to:
-1. Examine the code and all the errors in the list.
-2. Reason which errors from the list are included in the code and return all that apply based on your reasoning.
-3. Return an empty list if none of the errors are present in the code or the code is correct.
+    def contains_error(outputs, problems, error_ls, problem_errors_ls):
+#         system_content = """You are an experienced code reviewer. Given a programming problem along with a code and a list of errors, your task is to:
+# 1. Examine the code and all the errors in the list.
+# 2. Reason which errors from the list are included in the code and return all that apply based on your reasoning.
+# 3. Return an empty list if none of the errors are present in the code or the code is correct.
 
-Return ONLY valid JSON on a single line. Do not use markdown code fences. Do not include trailing comments.
-Output format: {"errors": [<error 1>, <error 2>, ...]}
+# Output rules (CRITICAL):
+# - Output MUST be valid JSON.
+# - Use DOUBLE QUOTES (") for all strings and keys.
+# - Do NOT use single quotes (').
+# - Do NOT include explanations, reasoning, comments, or extra text.
+# - Do NOT wrap the JSON in markdown or code blocks.        
+
+# The output MUST match this exact schema:
+# {"errors": ["error 1", "error 2", ...]}
+# """
+
+        system_content = """You are an experienced code reviewer. You will be provided with a programming problem along with a student code and a list of errors. Your task is to reason on the code and return all errors from the list that are included in the student code or an empty list if none of the errors are present in the code or the code is correct in JSON format.
+Output rules (CRITICAL):
+- Output MUST be valid JSON.
+- Use DOUBLE QUOTES (") for all strings and keys.
+- Do NOT use single quotes (').
+- Do NOT include explanations, reasoning, comments, or extra text.
+- Do NOT wrap the JSON in markdown or code blocks.        
+
+The output MUST match this exact schema:
+{"errors": ["error 1", "error 2", ...]}
 """
 
         prompt_ls = []
-        for output, problem, error_i in zip(outputs, problems, error_ls):
+        for output, problem, error_i in zip(outputs, problems, problem_errors_ls):
             user_prompt = f"Problem\n{problem}\n\nCode:\n{output}\n\nThe error list is:\n{error_i}" + "\n\nNow follow the instructions in system message, select all errors from the list that are present in the code in one line json format."
 
             message = [
@@ -197,7 +244,7 @@ Output format: {"errors": [<error 1>, <error 2>, ...]}
             prompt_ls.append(prompt)
 
         inputs = tokenizer(prompt_ls, return_tensors="pt", padding=True).to(model.device)
-        out = model.generate(**inputs, do_sample=False)
+        out = model.generate(**inputs, do_sample=False, max_new_tokens=60)
         
         generated = out[:, inputs["input_ids"].shape[1]:]
 
@@ -208,11 +255,19 @@ Output format: {"errors": [<error 1>, <error 2>, ...]}
 
         cleaned_out = [s.replace("```json", "").replace("```", "").strip() for s in decoded]
 
+
+        def iou(generated_code_ls, gt_code_ls):
+            set1, set2 = set(generated_code_ls), set(gt_code_ls)
+            intersection = set1 & set2
+            union = set1 | set2
+            return len(intersection) / len(union) if union else 0.0
+
+
         try:
             ans = [json.loads(s) for s in cleaned_out]
             error_res = [s['errors'] for s in ans]
+            error_reward = [iou(error_res_i, error_ls_i) for error_res_i, error_ls_i in zip(error_res, error_ls)]
 
-            error_reward = [len(err_i) / len(error_ls[i]) if len(error_ls[i]) > 0 else 0 for i, err_i in enumerate(error_res)]
         except Exception as e:
             print("Error in parsing JSON:", e)
             # print("Generated outputs:", cleaned_out)
@@ -256,7 +311,7 @@ Output format: {"errors": [<error 1>, <error 2>, ...]}
 
         return corr_reward
 
-    def reward_fn(completions, problem, errors, **kwargs):
+    def reward_fn(completions, problem, errors, problem_errors, **kwargs):
         # Two cases: if ground truth code is correct, use reward to check if the generated code is also correct, 1 for correct, 0 for incorrect
         if len(errors[0]) == 0:
             if len(errors[len(errors)//2]) == 0:
@@ -271,7 +326,9 @@ Output format: {"errors": [<error 1>, <error 2>, ...]}
                 second_sample_completions = completions[len(completions)//2:]
                 second_sample_prompts = problem[len(problem)//2:]
                 second_sample_errors = errors[len(errors)//2:]
-                error_reward = contains_error(second_sample_completions, second_sample_prompts, second_sample_errors)
+                second_sample_problem_errors = problem_errors[len(problem_errors)//2:]
+                
+                error_reward = contains_error(second_sample_completions, second_sample_prompts, second_sample_errors, second_sample_problem_errors)
 
                 total_reward = corr_reward + error_reward
                 return total_reward
@@ -282,7 +339,8 @@ Output format: {"errors": [<error 1>, <error 2>, ...]}
                 first_sample_completions = completions[:len(completions)//2]
                 first_sample_prompts = problem[:len(problem)//2]
                 first_sample_errors = errors[:len(errors)//2]
-                error_reward = contains_error(first_sample_completions, first_sample_prompts, first_sample_errors)
+                first_sample_problem_errors = problem_errors[:len(problem_errors)//2]
+                error_reward = contains_error(first_sample_completions, first_sample_prompts, first_sample_errors, first_sample_problem_errors)
 
                 second_sample_completions = completions[len(completions)//2:]
                 second_sample_prompts = problem[len(problem)//2:]
@@ -291,7 +349,7 @@ Output format: {"errors": [<error 1>, <error 2>, ...]}
                 total_reward = error_reward + corr_reward
                 return total_reward
             else:
-                error_reward = contains_error(completions, problem, errors)
+                error_reward = contains_error(completions, problem, errors, problem_errors)
                 return error_reward
 
     return reward_fn
@@ -322,7 +380,7 @@ def grpo(args, device, sft_time, grpo_time):
 
     # Use problem + ability level as prompt, need judge model to evaluate errors
     if args.with_knowledge:
-        judge_model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-Coder-14B-Instruct", dtype=torch.bfloat16).to(device)
+        judge_model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-Coder-7B-Instruct", dtype=torch.bfloat16).to(device)
         judge_model.eval()
         judge_tokenizer = create_tokenizer(args)
         judge_tokenizer.padding_side = "left"
@@ -334,10 +392,9 @@ def grpo(args, device, sft_time, grpo_time):
         valid_ds = load_student_ability_dataset(valid_set, tokenizer, args.prompt_column)
         test_ds = load_student_ability_dataset(test_set, tokenizer, args.prompt_column)
         error_reward = make_error_reward_fn(judge_model, judge_tokenizer)
-        # reward_functions = [match_ability_reward, diversity_reward, error_reward]
-        # reward_weights = [args.reward_weight, 1 - args.reward_weight, 1.0]
-        reward_functions = [diversity_reward, error_reward]
-        reward_weights = [1.0, 1.0]
+        reward_functions = [match_ability_reward, diversity_reward, error_reward]
+        reward_weights = [1.0, 1.0, 1.0]
+        
 
     else:
         train_ds = load_student_dataset(train_set, tokenizer, args.prompt_column)
@@ -357,17 +414,18 @@ def grpo(args, device, sft_time, grpo_time):
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         bf16=True,
         logging_steps=50,
-        save_strategy="steps",
-        save_steps=1000,
-        save_total_limit=3,
-        eval_strategy="steps",
-        eval_steps=1000,
+        save_strategy="epoch",
+        # save_steps=1000,
+        save_total_limit=1,
+        eval_strategy="epoch",
+        # eval_steps=1000,
         load_best_model_at_end=True,
         report_to="wandb" if args.log_wandb else "none",
         num_generations=args.num_generations,
         beta=args.beta,
         max_completion_length=args.max_new_tokens,
-        top_k=50,
+        temperature=0.7,
+        top_k=40,
         repetition_penalty=1.1,
         reward_weights=reward_weights,
     )
@@ -408,6 +466,10 @@ def eval_grpo(args, device, grpo_time):
     with open(os.path.join(args.model_save_dir, grpo_time, save_file), 'w') as f:
         json.dump(generated_output, f, indent=2)
     
+    with open(os.path.join(args.model_save_dir, grpo_time, save_file), 'r') as f:
+        generated_output = json.load(f)
+
+
     results = evaluate(test_code_ls, generated_output, test_problem_ls, args, grpo_time, lang='java')
     if args.k == 1:
         result = {'codeBLEU': results['codebleu']}
@@ -423,27 +485,28 @@ def eval_grpo(args, device, grpo_time):
     #     problems = generated_output['problems']
     #     generated_output = generated_output['generated_codes']
     
-    error_df = pd.read_pickle('data/data_inc_error_label_full.pkl')
+    # error_df = pd.read_pickle('data/data_inc_error_label_full.pkl')
 
-    test_set.drop(columns=['error_labels'], inplace=True)
-    eval_res = evaluate_error_coverage_individual(test_set, error_df, generated_output, args)
+    # test_set.drop(columns=['error_labels'], inplace=True)
+    # eval_res = evaluate_error_coverage_individual(test_set, error_df, generated_output, args)
 
-    with open(os.path.join(args.model_save_dir, grpo_time, 'llm_judge_res.pkl'), 'wb') as f:
-        pickle.dump(eval_res, f)
+    # with open(os.path.join(args.model_save_dir, grpo_time, 'llm_judge_res.pkl'), 'wb') as f:
+    #     pickle.dump(eval_res, f)
 
-    print('Finished LLM as judge evaluation for individual level error coverage')
+    # print('Finished LLM as judge evaluation for individual level error coverage')
 
-    print('Start LLM as judge overall evaluation')
+    # print('Start LLM as judge overall evaluation')
 
-    with open('data/problem_error_dict_30.pkl', 'rb') as f:
-        problem_error_dict = pickle.load(f)
+    # with open('data/problem_error_dict_0_30.pkl', 'rb') as f:
+    #     problem_error_dict = pickle.load(f)
 
-    output = evaluate_error_coverage_overall(test_set, problem_error_dict, generated_output, args)
+    # output = evaluate_error_coverage_overall(test_set, problem_error_dict, generated_output, args)
     
-    with open(os.path.join(args.model_save_dir, grpo_time, 'llm_judge_overall_res.pkl'), 'wb') as f:
-        pickle.dump(output, f)
+    # with open(os.path.join(args.model_save_dir, grpo_time, 'llm_judge_overall_res.pkl'), 'wb') as f:
+    #     pickle.dump(output, f)
     
-    print('Finished LLM as judge overall evaluation')
+    # set_trace()
+    # print('Finished LLM as judge overall evaluation')
 
 
 
@@ -453,7 +516,7 @@ def main():
     parser.add_argument("--prompt_column", type=str, default="prompt", help="Column name for prompts in the data.")
     parser.add_argument("--test_size", type=float, default=0.2, help="Proportion of the dataset to include in the test split.")
     parser.add_argument("--random_state", type=int, default=0, help="Random state for data splitting.")
-    parser.add_argument("--model_save_dir", type=str, default='sft_checkpoints', help="Directory to save the model checkpoints.")
+    parser.add_argument("--model_save_dir", type=str, default='sft_checkpoints', help="Directory to load the sft model checkpoints and load grpo model for eval.")
     parser.add_argument("--base_model", type=str, default="Qwen/Qwen2.5-Coder-7B-Instruct", help="Pretrained model name or path.")
     parser.add_argument("--output_dir", type=str, default="grpo_checkpoints", help="Output directory for GRPO training.")
     parser.add_argument("--learning_rate", type=float, default=1e-7, help="Learning rate for training.")
@@ -462,9 +525,10 @@ def main():
     parser.add_argument("--gradient_accumulation_steps", type=int, default=1, help="Number of gradient accumulation steps.")
     parser.add_argument("--num_generations", type=int, default=5, help="Number of generations for GRPO.")
     parser.add_argument("--k", type=int, default=1, help="Number of generations per sample for inference.")
-    parser.add_argument("--max_new_tokens", type=int, default=200, help="Maximum completion length for generation.")
+    parser.add_argument("--max_new_tokens", type=int, default=400, help="Maximum completion length for generation.")
     parser.add_argument("--log_wandb", action='store_true', help="Whether to log training with Weights & Biases.")
     parser.add_argument("--beta", type=float, default=0.0, help="Beta parameter for GRPO.")
+    parser.add_argument("--wandb_key", type=str, default='ff70920d9852a9d2e78bbd1cd2e100154d2c9c7d', help="API key for Weights & Biases.")
     parser.add_argument("--split_by", type=str, default='student', help="Split data by 'student' or 'problem'.")
     parser.add_argument("--reward_weight", type=float, default=1, help="Reward function weights for GRPO.")
     parser.add_argument("--with_knowledge", action='store_true', help="Whether to include knowledge component information in prompts.")
@@ -476,22 +540,21 @@ def main():
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
+    sft_student_time = '20251219_235528' # split by student with knowledge notrunc
+    # sft_problem_time = '20251219_194440'  # split by problem with knowledge notrunc
 
-    # sft_time = '20251130_211626' # split by student
+    # grpo_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # print(grpo_time)
 
-    # sft_time = '20251206_070913' # split by problem
+    # grpo(args, device, sft_student_time, grpo_time)
 
-    sft_student_time = '20251204_154225_student_knowledge' # split by student with knowledge
+    args.model_save_dir = args.output_dir
+    # grpo_time = '20251220_195626' # grpo with student knowledge split by problem
 
-    grpo_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    print(grpo_time)
+    grpo_time = '20251223_163015' # grpo with student knowledge split by student
+    eval_grpo(args, device, grpo_time)
 
-    grpo(args, device, sft_student_time, grpo_time)
 
-    # args.model_save_dir = args.output_dir
-
-    # grpo_time = '20251215_212503' # grpo with student knowledge split by student
-    # eval_grpo(args, device, grpo_time)
 
 if __name__ == "__main__":
     main()
